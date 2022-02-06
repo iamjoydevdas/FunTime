51af6960c58bf94cb7508592e4d29893a50f2e05
Joydev Das <iamjoydevdas@gmail.com>
2022-02-06 20:45:11 +0530

    common feign

--- funtime/booking-feign/src/main/java/com/example/BookingFeign.java
+++ funtime/booking-feign/src/main/java/com/example/BookingFeign.java
@@ -1,10 +1,10 @@
-package com.example.srchmgnt.api;
+package com.example;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name="booking-management")
-public interface FeignEg {
+public interface BookingFeign {
 	@GetMapping("/Ok")
 	public String ok();
 }
--- funtime/srchmgnt/src/main/java/com/example/srchmgnt/SearchManagement.java
+++ funtime/srchmgnt/src/main/java/com/example/srchmgnt/SearchManagement.java
@@ -6,24 +6,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
-import com.example.srchmgnt.api.FeignEg;
-import com.example.srchmgnt.repo.ISearchRepo;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import com.example.BookingFeign;
 
 /**
  * Hello world!
  *
  */
-@EnableFeignClients
+@EnableFeignClients(basePackages="com.example")
 @ComponentScan(basePackages="com.example")
 @SpringBootApplication
 public class SearchManagement implements CommandLineRunner
 {
 	@Autowired
-	private FeignEg searchRepo;
+	private BookingFeign searchRepo;
 	
     public static void main( String[] args )
     {
@@ -33,7 +29,5 @@ public class SearchManagement implements CommandLineRunner
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println(searchRepo.ok());
-		searchRepo.ok();
-		
 	}
 }
